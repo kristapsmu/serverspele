7ff930a99f96e84fe90cfeff0c32f1e4ca91c2da
km <nav@nav.lv>
2019-12-12 19:07:23 +0200

    test2
    test

--- main.py
+++ main.py
@@ -20,34 +20,10 @@ produkti = {
 @app.route('/')
 def home():
   return render_template("home.html", name = "PARAMETRS")
-@app.route('/about')
-def about():
-  return render_template("about.html")
-@app.route('/contact')
-def contact():
-  return render_template("contact.html")
-@app.route('/shop')
-def shop():
-  return render_template("shop.html",produkts = produkti,produktugarums=len(produkti),summa = 0)
-@app.route('/prametri')
-def prametri():
-  parametri = request.args
-   
-  return (json.dumps(parametri, sort_keys=True))
-@app.route('/nauda')
-def nauda():
-  r = ""
-  url = 'https://api.exchangerate-api.com/v4/latest/EUR'
-  headers = {'content-type': 'application/json'}
-  r = requests.post(url, '', headers=headers)
-  uzjson = r.json()
-  return render_template("nauda.html",masivs = uzjson)
-@app.route('/bilde')
-def bilde():
-  r = ""
-  url = 'https://dog.ceo/api/breeds/image/random'
-  headers = {'content-type': 'application/json'}
-  r = requests.get(url, '', headers=headers)
-  uzjson = r.json()
-  return render_template("bilde.html",masivs = uzjson)
+@app.route('/ajax')
+def ajax():
+ 	
+  return render_template("ajax.html")
+ 
+ 
 app.run(host='0.0.0.0',port= 8020)
